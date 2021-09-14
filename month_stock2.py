a7355df77ca43371ed8d772c94e250ddb1bc09aa
Fayuan Chu <acsx7339@gmail.com>
2021-09-15 00:33:26 +0800

    modify

--- month_stock2.py
+++ month_stock2.py
@@ -17,7 +17,7 @@ from bs4 import BeautifulSoup
 import shutil
 from selenium.common.exceptions import WebDriverException
 
-
+counter = 0
 def goodinfo():
     headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"}
     url = "https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E8%87%AA%E8%A8%82%E7%AF%A9%E9%81%B8&INDUSTRY_CAT=%E6%88%91%E7%9A%84%E6%A2%9D%E4%BB%B6&FL_ITEM0=%E5%88%86%E7%B4%9A%E6%8C%81%E6%9C%89%E6%AF%94%E4%BE%8B%E9%80%B1%E5%A2%9E%E6%B8%9B%E6%95%B8%E2%80%9350%E5%BC%B5%E4%BB%A5%E4%B8%8B&FL_VAL_S0=&FL_VAL_E0=0&FL_ITEM1=%E5%88%86%E7%B4%9A%E6%8C%81%E6%9C%89%E6%AF%94%E4%BE%8B%E9%80%B1%E5%A2%9E%E6%B8%9B%E6%95%B8%E2%80%93%E8%B6%85%E9%81%8E400%E5%BC%B5&FL_VAL_S1=0&FL_VAL_E1=&FL_ITEM2=&FL_VAL_S2=&FL_VAL_E2=&FL_ITEM3=&FL_VAL_S3=&FL_VAL_E3=&FL_ITEM4=&FL_VAL_S4=&FL_VAL_E4=&FL_ITEM5=&FL_VAL_S5=&FL_VAL_E5=&FL_ITEM6=&FL_VAL_S6=&FL_VAL_E6=&FL_ITEM7=&FL_VAL_S7=&FL_VAL_E7=&FL_ITEM8=&FL_VAL_S8=&FL_VAL_E8=&FL_ITEM9=&FL_VAL_S9=&FL_VAL_E9=&FL_ITEM10=&FL_VAL_S10=&FL_VAL_E10=&FL_ITEM11=&FL_VAL_S11=&FL_VAL_E11=&FL_RULE0=&FL_RULE1=&FL_RULE2=&FL_RULE3=&FL_RULE4=&FL_RULE5=&FL_RANK0=&FL_RANK1=&FL_RANK2=&FL_RANK3=&FL_RANK4=&FL_RANK5=&FL_FD0=&FL_FD1=&FL_FD2=&FL_FD3=&FL_FD4=&FL_FD5=&FL_SHEET=%E5%B9%B4%E7%8D%B2%E5%88%A9%E8%83%BD%E5%8A%9B&FL_SHEET2=%E7%8D%B2%E5%88%A9%E8%83%BD%E5%8A%9B&FL_MARKET=%E4%B8%8A%E5%B8%82%2F%E4%B8%8A%E6%AB%83&FL_QRY=%E6%9F%A5++%E8%A9%A2"
@@ -41,7 +41,7 @@ def open_fugle(stockList):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--proxy-server=10.243.17.28') 
+    # chrome_options.add_argument('--proxy-server=10.243.17.28') 
     folder = datetime.datetime.today().strftime('%Y%m%d')
     url = 'https://www.fugle.tw' 
     driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
@@ -64,7 +64,7 @@ def open_fugle(stockList):
         os.makedirs(dir)
     for item in stockList:
         try:
-            # print('stock: ',item)
+            print('stock: ',item)
             WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.ID, "ember19")))
             input_element = driver.find_element_by_id("ember19")
             input_element.send_keys(item)
@@ -93,17 +93,25 @@ def open_fugle(stockList):
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "ember19")))
             driver.find_element_by_id("ember19").clear()
         except WebDriverException:
-            temp = stockList.index((item))
-            new_list = stockList[temp:]
-            print('start new because fail',item)
-            open_fugle(new_list)
+            global counter
+            counter +=1
+            if counter < 20:
+                temp = stockList.index((item))
+                new_list = stockList[temp:]
+                print('start new because fail',item)
+                time.sleep(10)
+                open_fugle(new_list)
+            else:
+                print('break from the stock: ', item)
+                break
         except:
             fail_list.append(item)
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "ember19")))
             driver.find_element_by_id("ember19").clear()
     with open('fail.txt','w') as file:
-        file.write(fail_list)
+        file.write(str(fail_list))
         print('fail_list, ',fail_list)
+        
 
 if __name__ == "__main__":
     st_list = goodinfo()
